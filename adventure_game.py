d641fc6c99ca8e5606009bb76a208d9e45852223
ADPriceless <adam.price500@outlook.com>
2024-01-18 17:20:55 +0700

    add logic to setup the in-game map (the house)

--- adventure_game.py
+++ adventure_game.py
@@ -1,6 +1,19 @@
+def reshape2d(one_dim: list) -> list[list]:
+    '''reshape 1d `list` into 2d `list`. `one_dim` must have even number of elements'''
+    length = len(one_dim)
+    assert length % 2 == 0
+    two_dim = [one_dim[:length//2], one_dim[length//2:]]
+    return two_dim
+
+
 class HauntedHouse:
-    def __init__(self) -> None:
+    def __init__(self, 
+                 rooms: list[str],
+                 room_descriptions: dict[str, str]
+    ) -> None:
         self.player_x, self.player_y = 0, 0
+        self.house = reshape2d(rooms)
+        self.rooms_descriptions = room_descriptions
         self.playing = False
 
     def _show_intro(self) -> None:
@@ -28,7 +41,11 @@ class HauntedHouse:
 
 
 def main():
-    haunted_house = HauntedHouse()
+    ROOMS = ('Kitchen', 'Living Room','Dining Room', 'Toilet')
+    DESCRIPTIONS = ROOMS
+    room_descriptions = {room: desc for (room, desc) in zip(ROOMS, DESCRIPTIONS)}
+
+    haunted_house = HauntedHouse(ROOMS, room_descriptions)
     haunted_house.play()
 
 
